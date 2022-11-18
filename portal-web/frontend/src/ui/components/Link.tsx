f221a704a1f27d94d13df08055fb9195e7baf4b4
Arthur Guerra <conta.arthurguerra@gmail.com>
2022-11-18 16:29:42 -0300

    refactor(frontend)cleans frontend folder to use nextjs

--- portal-web/frontend/src/ui/components/Link.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const StyledLink = styled(Link)`
-  text-decoration: underline;
-  color: #f63e02;
-`;
-
-export default StyledLink;
--- portal-web/frontend/src/ui/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 2em;
-  text-align: center;
-  color: #ff4438;
-`;
-
-export default Title;
