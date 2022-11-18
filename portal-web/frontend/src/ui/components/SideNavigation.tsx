8728deb796a089684d693acb44be432afe1bb70a
Arthur Guerra <conta.arthurguerra@gmail.com>
2022-11-18 07:43:52 -0300

    feat(frontend): change route by side nav

--- portal-web/frontend/src/ui/components/SideNavigation.tsx
+++ portal-web/frontend/src/ui/components/SideNavigation.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 type SideItemProps = {
   selected?: boolean;
   expanded?: boolean;
+  onClick?: () => void;
 };
 
 type SideNavigationProps = {
--- portal-web/frontend/src/ui/layouts/ContributorLayout/index.tsx
+++ portal-web/frontend/src/ui/layouts/ContributorLayout/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { FaUsers, FaUserPlus } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
+import { CgPushChevronLeft, CgPushChevronRight } from "react-icons/cg";
 
 import { SideNavigation, SideItem } from "../../components/SideNavigation";
-import { FaUsers, FaUserPlus } from "react-icons/fa";
-import { CgPushChevronLeft, CgPushChevronRight } from "react-icons/cg";
 
 const LayoutContainer = styled.div`
   display: flex;
@@ -12,30 +13,32 @@ const LayoutContainer = styled.div`
 `;
 
 const ContributorLayout = ({ children }: { children: React.ReactNode }) => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
   const [expanded, setExpanded] = useState(true);
 
   return (
     <LayoutContainer>
       <SideNavigation expanded={expanded}>
-        <SideItem selected={true}>
+        <SideItem
+          selected={pathname === "/clients"}
+          onClick={() => navigate("/clients")}
+        >
           <FaUsers size="2em" />
           {expanded ? <span>Clientes</span> : null}
         </SideItem>
-        <SideItem selected={false}>
+        <SideItem
+          selected={pathname === "/addClient"}
+          onClick={() => navigate("/addClient")}
+        >
           <FaUserPlus size="2em" />
           {expanded ? <span>Novo cliente</span> : null}
         </SideItem>
-        <SideItem>
+        <SideItem onClick={() => setExpanded(!expanded)}>
           {expanded ? (
-            <CgPushChevronLeft
-              size="1.75em"
-              onClick={() => setExpanded(false)}
-            />
+            <CgPushChevronLeft size="1.75em" />
           ) : (
-            <CgPushChevronRight
-              size="1.75em"
-              onClick={() => setExpanded(true)}
-            />
+            <CgPushChevronRight size="1.75em" />
           )}
         </SideItem>
       </SideNavigation>
