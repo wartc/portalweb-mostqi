1a76234c3cf795d235c5cb4aa1afee7389a881ed
Arthur Guerra <conta.arthurguerra@gmail.com>
2022-11-18 20:57:20 -0300

    feat(frontend): improve navigation ux

--- portal-web/frontend/layouts/AuthorizedLayout/index.tsx
+++ portal-web/frontend/layouts/AuthorizedLayout/index.tsx
@@ -1,10 +1,11 @@
 import { FaAddressCard, FaUserPlus, FaUsers } from "react-icons/fa";
-import Link from "next/link";
 
 import styles from "./AuthorizedLayout.module.scss";
 import { useAuth } from "../../contexts/AuthContext";
+import { useRouter } from "next/router";
 
 const AuthorizedLayout = ({ children }: { children: React.ReactNode }) => {
+  const { push, pathname } = useRouter();
   const { user } = useAuth();
 
   console.log(user!.type); // todo: render different menu items based on user type
@@ -13,24 +14,33 @@ const AuthorizedLayout = ({ children }: { children: React.ReactNode }) => {
     <div className={styles.container}>
       <nav className={styles.sideNavigation}>
         <ul className={styles.sideNavigationList}>
-          <li className={`${styles.sideNavigationListItem} ${styles.selected}`}>
+          <li
+            className={`${styles.sideNavigationListItem} ${
+              pathname === "/clients" ? styles.selected : ""
+            }`}
+            onClick={() => push("/clients")}
+          >
             <FaUsers size="2em" />
-            <Link className={styles.itemLink} href="/clients">
-              Clientes
-            </Link>
+            <span className={styles.itemLink}>Clientes</span>
           </li>
-          <li className={styles.sideNavigationListItem}>
+          <li
+            className={`${styles.sideNavigationListItem} ${
+              pathname === "/clients/new" ? styles.selected : ""
+            }`}
+            onClick={() => push("/clients/new")}
+          >
             <FaUserPlus size="2em" />
-            <Link className={styles.itemLink} href="/clients/new">
-              Novo cliente
-            </Link>
+            <span className={styles.itemLink}>Novo cliente</span>
           </li>
 
-          <li className={styles.sideNavigationListItem}>
+          <li
+            className={`${styles.sideNavigationListItem} ${
+              pathname === "/profile" ? styles.selected : ""
+            }`}
+            onClick={() => push("/profile")}
+          >
             <FaAddressCard size="2em" />
-            <Link className={styles.itemLink} href="/profile">
-              Perfil
-            </Link>
+            <span className={styles.itemLink}>Perfil</span>
           </li>
         </ul>
       </nav>
