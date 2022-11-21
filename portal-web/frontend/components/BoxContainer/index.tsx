b33ede026982610dba0a6c080744bca6cd256fa5
Arthur Guerra <conta.arthurguerra@gmail.com>
2022-11-20 21:56:32 -0300

    fix: optional classname in boxcontainer

--- portal-web/frontend/components/BoxContainer/index.tsx
+++ portal-web/frontend/components/BoxContainer/index.tsx
@@ -2,10 +2,10 @@ import styles from "./BoxContainer.module.scss";
 
 const BoxContainer = ({
   children,
-  className,
+  className = "",
 }: {
   children: React.ReactNode;
-  className: string;
+  className?: string;
 }) => {
   return <div className={`${styles.boxContainer} ${className}`}>{children}</div>;
 };
